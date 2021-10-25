8523b88e95fe44a7d5524d220a6b81707d204700
r1ngis <and.ringstad@hotmail.com>
2021-10-25 16:23:32 +0200

    Changed Hello World to use PHP

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-Hello World!
+<?php
+
+namespace mvc;
+
+class XBlock{
+
+    protected $field0 = "f0";
+    private $field1 = "f1";
+    public $field2 = "f2";
+}
+
+$block = new XBlock();
+echo $block->field2;
+
+?>
