1213c66ae261509891965ccb95777618a01682d4
emilbratt <emilbratt@gmail.com>
2021-10-31 21:02:46 +0100

    establish route

--- index.php
+++ index.php
@@ -1,22 +1,5 @@
 <?php
-function br () {
-  echo '<br>';
-}
-echo 'This is the controller for the MVC';
-br();
-echo 'PHP Version: ' . phpversion();
-br();
-echo 'Script Location: ' . __FILE__;
-br();
-echo 'Request URI: ' . $_SERVER['REQUEST_URI'];
-br();
-echo 'Query String: ' . $_SERVER['QUERY_STRING'];
-br();
-if (array_key_exists('REDIRECT_QUERY_STRING', $_SERVER)) {
-  echo 'Redirect Query String: ' . $_SERVER['REDIRECT_QUERY_STRING'];
-}
-else {
-  echo 'Redirect Query String: None';
-}
-br();
-echo 'checkout branch file test';
+
+require_once 'core/lib/Router.php';
+
+$request = new Route;
